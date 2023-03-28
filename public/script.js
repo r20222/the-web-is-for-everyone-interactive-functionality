2c12d7e012654003d20533b8da38ce8dd75edfdc
r20222 <101579892+r20222@users.noreply.github.com>
2023-03-28 15:40:58 +0200

    notitie-slider
    mobile-only

--- public/script.js
+++ public/script.js
@@ -21,3 +21,42 @@ function menuAway(){
     article.classList.remove('pointer-event-none')
 }
 
+// notitie carrousel
+
+// definieer de variabelen
+const buttonLeft = document.querySelector('.back')
+const buttonRight = document.querySelector('.next')
+
+// laat de slide bij de eerste beginnen
+let slideIndex = 1;
+
+// laat de functie showdivs in actie treden (als de slideindex 1 is, zie je de eerste 'tip').
+showDivs(slideIndex);
+
+// Geef de knoppen een eventlistener
+buttonLeft.addEventListener('click', function() { plusClick(-1) });
+buttonRight.addEventListener('click', function() { plusClick(1) });
+
+// deze functie wordt uitgevoerd wanneer je op de button klikt, 
+// Met showDivs(slideIndex += n); geeft die aan of de slideindex een pagina verder of terug moet gaan.
+function plusClick(n){
+    showDivs(slideIndex += n);
+    console.log('clicky')
+}
+
+// Deze onderstaand functie showdivs besluit welke 'tip' te zien is.
+// n staat voor de slideindex waarde
+// x staat voor de slides
+// met de i wordt er een loop uitgevoerd, i gaat gewoon getallen langs, en zolang het getal kleiner is dan
+// de lengte van het aantal slider wordt er een nummer bij opgeteld
+// [slideIndex-1] wordt gebruikt omdat bijvoorbeeld +3 eigenlijk overeenzou komen met index 2.
+function showDivs(n) {
+    let i;
+    let x = document.getElementsByClassName("notitie-slider");
+    if (n > x.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = x.length}
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";  
+    }
+    x[slideIndex-1].style.display = "block";  
+  }
