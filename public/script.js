af26039b45ff603391235329d8423287d7aa28bd
r20222 <101579892+r20222@users.noreply.github.com>
2023-04-04 14:47:43 +0200

    js disabled desktop

--- public/script.js
+++ public/script.js
@@ -56,9 +56,9 @@ function showDivs(n) {
     if (n > x.length) {slideIndex = 1}
     if (n < 1) {slideIndex = x.length}
     for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";  
+        x[i].classList.remove("show-slider");
     }
-    x[slideIndex-1].style.display = "block";  
+    x[slideIndex-1].classList.add("show-slider");
   }
 
 
